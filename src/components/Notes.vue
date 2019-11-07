b1d3e7c425acc8e3aafe36232b65cb939d380fdd
sophi-e <sophierithous@gmail.com>
2019-11-07 10:12:23 +0100

    added note component

---
+++ src/components/Notes.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="note-div">
+        <form @submit.prevent='addNote'>
+            <label>Note Title</label>
+            <input type="text" v-model='note.title'>
+            <label>Note Text</label>
+            <textarea v-model='note.text'></textarea>
+            <button>Add Note</button>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    name: 'note-div',
+    data(){
+        return{
+            note: {
+                title: '',
+                text: ''
+            },
+            notes:[],
+        }
+    },
+    methods:{
+        addNote(){
+            let {title, text} = this.note;
+            this.notes.push({
+                title,
+                text,
+                date: new Date(Date.now()).toLocaleString()
+            })
+            this.note = {}
+        }
+    }
+}
+</script>
+<style>
+
+</style>
